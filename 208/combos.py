06145059541a3a7148ca58219c468e8506d95015
Volkmar Petschnig <volkmar.petschnig@protonmail.com>
2019-08-04 14:49:01 +0200

    Working solution for bite 208

--- 208/combos.py
+++ 208/combos.py
@@ -1,2 +1,14 @@
 def find_number_pairs(numbers, N=10):
-    pass
+    result = []
+    for x in numbers:
+        for y in numbers:
+           if x != y:
+               if x + y == N and (y,x) not in result:
+                   result.append((x,y))
+    return result
+
+
+if __name__ == '__main__':
+    input_list = [9,1,3,8,7]
+    print(find_number_pairs(input_list))
+
