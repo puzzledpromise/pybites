bd5da00e926a63e4d0221df0da62da18e2edb77c
Volkmar Petschnig <volkmar.petschnig@protonmail.com>
2019-08-08 12:47:33 +0200

    submission Bite 20 @ codechalleng.es

--- 20/account.py
+++ 20/account.py
@@ -15,3 +15,15 @@ class Account:
 
     # add 2 dunder methods here to turn this class 
     # into a 'rollback' context manager
+
+    def __enter__(self):
+        self.__account = Account()
+        self.__account._transactions = self._transactions[:]
+        return self.__account
+
+    def __exit__(self, *args):
+        if self.__account.balance >= 0:
+            self._transactions = self.__account._transactions[:]
+        self.__account = None
+
+
