e8ad73fff556bfdca379e4309781a05c664c744a
Volkmar Petschnig <volkmar.petschnig@protonmail.com>
2019-10-16 18:57:05 +0200

    submission Bite 186 @ codechalleng.es

--- 186/books.py
+++ 186/books.py
@@ -14,14 +14,18 @@ def get_number_books_read(books_per_year_goal: int,
        If books_per_year_goal negative or 0, or at_date is in the
        past, raise a ValueError."""
     at_date = at_date or str(NOW)
-    # TODOs
+    dt = parse(at_date)
+    
+    if books_per_year_goal <= 0:
+        raise ValueError(("Expected books_per_year_goal > 0 but"
+        f"got {books_per_year_goal}!"))
 
-    # 1. use dateutil's parse to convert at_date into a
-    # datetime object
+    if dt < NOW:
+        raise ValueError("The date lies in the past!")
+    
+    week_of_the_year = dt.isocalendar()[1]
+    offset = int(week_of_the_year / WEEKS_PER_YEAR * books_per_year_goal)
+    return offset
 
-    # 2. check books_per_year_goal and at_date and raise
-    # a ValueError if goal <= 0 or at_date in the past (< NOW)
-
-    # 3. check the offset of at_date in the year ("week of the
-    # year" - e.g. whatweekisit.com) and based on the books_per_year_goal,
-    # calculate the number of books that should have been read / completed
+if __name__ == "__main__":
+        print(get_number_books_read(52, str(datetime(2019,10,16))))
